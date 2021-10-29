9623f4d981c686977667d5552354f55fd6291195
Ludovic Marcoux Hoyos <ludomarcoux098@gmail.com>
2021-10-29 19:27:48 -0400

    Added Vet Class

---
+++ AssignmentTwo/AssignmentTwo/Vet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AssignmentTwo
+{
+    class Vet
+    {
+            [PrimaryKey, AutoIncrement]
+            public int ID { get; set; }
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string phoneNumber { get; set; }
+
+            public string email { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public String Specialization { get; set; }
+
+
+            public string IsValid()
+            {
+                if (firstName == null) return "First name not set";
+
+                if (lastName == null) return "Last name not set";
+
+                if (phoneNumber == null) return "Phone # not set";
+
+                if (email == null) return "email not set";
+
+                if (Specialization == null) return "Specialization not set";
+
+                if (Username == null) return "Username not set";
+
+                if (Password.Length < 10) return "Password needs to be at least 10 characters";
+
+                return null;
+
+                return $"First name: {firstName}\nLast name: {lastName:C}\nEmail: {email}\nPhone Number: {phoneNumber}\nSpecialization: {Specialization}";
+            }
+    }
+}
