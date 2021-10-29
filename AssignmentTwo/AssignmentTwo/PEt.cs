36f4bac36b4eedc1108a114eaa8fee19986984d7
Ludovic Marcoux Hoyos <ludomarcoux098@gmail.com>
2021-10-29 19:33:01 -0400

    Added Pet class

---
+++ AssignmentTwo/AssignmentTwo/PEt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+namespace AssignmentTwo
+{
+    class Pet
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string petType { get; set; }
+        public int age { get; set; }
+        public int OwnerTag { get; set; }
+
+
+        public string IsValid()
+        {
+            if (Name == null) return "Name not set";
+
+            if (petType == null) return "Pet Type not set";
+
+            if (age == null) return "Age not set";
+
+            if (OwnerTag == null) return "Owner tag not set";
+
+            return null;
+
+            return $"Name: {Name}\nPet Type: {petType:C}\nAge: {age}\nOwner Tag: {OwnerTag}";
+        }
+    }
+}
