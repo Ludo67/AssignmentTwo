7f6ab5dfb3079fa686e0fb9b399ad56058818e04
Ludovic <ludomarcoux098@gmail.com>
2021-10-29 13:11:58 -0400

    Added Navigation for the Login Page

--- AssignmentTwo/AssignmentTwo/MainPage.xaml.cs
+++ AssignmentTwo/AssignmentTwo/MainPage.xaml.cs
@@ -14,5 +14,20 @@ namespace AssignmentTwo
         {
             InitializeComponent();
         }
+
+        private async void loginButton_OnClicked(object sender, EventArgs e)
+        {
+            //VetRegisterPage = new NavigationPage(new VetRegistration());
+            //PetRegisterPage = new NavigationPage(new PetRegistration());
+            //Home = new NavigationPage(new HomePage());
+            //Registerpage = new NavigationPage(new Register());
+            await Navigation.PushAsync(new NavigationPage(new HomePage()));
+        }
+
+        private async void registerButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage(new Register()));
+        }
+
     }
 }
