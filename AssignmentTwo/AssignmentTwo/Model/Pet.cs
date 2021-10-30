5c6727e7d55ff7386e778f1027ee4f2004db6a38
LudoPoop <debopvp@gmail.com>
2021-10-29 21:40:39 -0400

    Refactored

--- AssignmentTwo/AssignmentTwo/Model/Pet.cs
+++ AssignmentTwo/AssignmentTwo/Model/Pet.cs
@@ -8,7 +8,6 @@ namespace AssignmentTwo
 {
     class Pet
     {
-
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
         public string Name { get; set; }
--- AssignmentTwo/AssignmentTwo/Model/User.cs
+++ AssignmentTwo/AssignmentTwo/Model/User.cs
@@ -12,11 +12,8 @@ namespace AssignmentTwo
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
-
         public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-
-        public int? OwnerTag { get; set; }
     } 
 }
