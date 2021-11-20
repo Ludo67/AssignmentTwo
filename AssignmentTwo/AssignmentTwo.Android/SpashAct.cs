42df3e9b0d3be118a2f0e4be5db78e9d5f0b3f8d
Ludovic Marcoux Hoyos <ludomarcoux098@gmail.com>
2021-11-19 23:22:15 -0500

    Implemented Splash Screen

---
+++ AssignmentTwo/AssignmentTwo.Android/SpashAct.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AssignmentTwo.Droid
+{
+    [Activity(Label = "Nov08", Icon = "@mipmap/icon", NoHistory = true,
+        Theme = "@style/MyTheme.Splash", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize |
+        ConfigChanges.Orientation | ConfigChanges.UiMode |
+        ConfigChanges.ScreenLayout |
+        ConfigChanges.SmallestScreenSize)]
+
+    class SpashAct: Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Thread.Sleep(1000);
+            Finish();
+        }
+        public override void OnBackPressed()
+        {
+            //base.OnBackPressed();
+        }
+    }
+}
